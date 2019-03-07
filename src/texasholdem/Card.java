aa203184e8724f345ecf3862ca9f7c48bfba3ed6
Kuangtutu <chanyummy0806@gmail.com>
2019-03-07 18:07:54 -0500

    Card.java set the base of every card

--- src/texasholdem/Card.java
+++ src/texasholdem/Card.java
@@ -1,26 +1,95 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
 package texasholdem;
 
-/**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- * @author dancye, 2018
- */
-public abstract class Card 
-{
-    //default modifier for child classes
-    
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    
+import com.sun.istack.internal.NotNull;
+
+import java.util.Comparator;
+
+public class Card implements Comparable<Card>{
+    private final Suit SUIT;
+    private final Value VALUE;
+    private final int INDEX;
+    private boolean faceUp;
+
     @Override
-    public abstract String toString();
-    
+    public int compareTo(Card card) {
+        return Comparators.INDEX.compare(this, card);
+    }
+
+    public Card(@NotNull int vIndex, int sIndex) {
+        this(sIndex + vIndex * 10);
+    }
+
+    public Card(@NotNull int index){
+        this.INDEX = index;
+        SUIT = Suit.values()[index%10];
+        VALUE = Value.values()[(index - (index % 10))/10];
+        faceUp = false;
+    }
+
+    public Suit getSuit() {
+        return SUIT;
+    }
+
+    public Value getValue() {
+        return VALUE;
+    }
+
+    public int getINDEX() {
+        return INDEX;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+
+    public String printCard(boolean isForceViewCard) {
+        if (isForceViewCard) {
+            return this.SUIT.toString()+this.VALUE.toString();
+        }
+        if (!isFaceUp()) {
+            return "🎴";
+        }
+        return this.SUIT.toString()+this.VALUE.toString();
+    }
+
+    public String printCardText(boolean isForceViewCard) {
+        if (isForceViewCard) {
+            return this.SUIT.toASCII()+this.VALUE.toString();
+        }
+        if (!isFaceUp()) {
+            return "Card";
+        }
+        return this.SUIT.toASCII()+this.VALUE.toString();
+    }
+
+    @Override
+    public String toString() {
+        return printCard(false);
+    }
+
+    public static class Comparators {
+        public static Comparator<Card> INDEX = new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                return o2.INDEX - o1.INDEX ;
+            }
+        };
+        public static Comparator<Card> SUIT = new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                return  o2.SUIT.ordinal() - o1.SUIT.ordinal();
+            }
+        };
+        public static Comparator<Card> VALUE = new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                return o2.VALUE.ordinal() - o1.VALUE.ordinal() ;
+            }
+        };
+    }
 }
+
