9cee240dec6f7e7c50a49609e3373b728c4feda4
Yiyao Zhang <zhang815605562@gmail.com>
2019-04-19 22:21:58 -0400

    More instructions on ascII mode
    Signed-off-by: Yiyao Zhang <zhang815605562@gmail.com>

--- src/texasholdem/MainGame.java
+++ src/texasholdem/MainGame.java
@@ -5,7 +5,7 @@ public class MainGame {
         CommandPrompt game = new CommandPrompt();
         InputHandleSystem input = new InputHandleSystem();
 
-        System.out.println("Texas Hold'em\ntype \"set [numbersOfPlayers]\" to set the amount of players (default: 4)\nthen type \"srun start\" to run. Or type \"help\" for more command.\nrules can be found here: https://en.wikipedia.org/wiki/Texas_hold_%27em#Rules");
+        System.out.println("Texas Hold'em\ntype \"set [numbersOfPlayers]\" to set the amount of players (default: 4)\nthen type \"srun start\" to run. Or type \"help\" for more command.\nrules can be found here: https://en.wikipedia.org/wiki/Texas_hold_%27em#Rules\nimportant! If you found some character display not as expect, please type \"ascii\" to enter ascII mode before you start the game!");
         game.viewGameTable();
         while (!game.isEnded()) {
             game.comm(input.getLine("game:\\"));
